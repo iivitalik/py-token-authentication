e4bdb817c75b42f076d88d1db59d95be7a52e156
Vitalik <lg.mobile0808@gmail.com>
2025-10-15 14:21:59 +0300

    flake

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,5 @@ from user.views import CreateUserView
 app_name = "user"
 
 urlpatterns = [
-   path("register/", CreateUserView.as_view(), name="create"),
+    path("register/", CreateUserView.as_view(), name="create"),
 ]
