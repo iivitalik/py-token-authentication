76cb3d95c9fec477067232da7dd05b4444dfb437
Vitalik <lg.mobile0808@gmail.com>
2025-10-22 15:19:45 +0300

    sol

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -6,11 +6,18 @@ class IsAdminOrIfAuthenticatedReadOnly(permissions.BasePermission):
     Custom permission to allow:
     - Read-only for authenticated users
     - Full access for admin users
+    - Allow POST (create) for unauthenticated users (user registration)
     """
 
     def has_permission(self, request, view):
+        # Allow POST for unauthenticated users (user registration)
+        if request.method == "POST":
+            return True
+
+        # Allow read-only for authenticated users
         if (request.method in permissions.SAFE_METHODS
                 and request.user.is_authenticated):
             return True
 
+        # Allow full access for admin users
         return bool(request.user and request.user.is_staff)
